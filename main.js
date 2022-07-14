8dc88b1685efa78421d69dcb98fd97b2210874a9
Nikolay Naidenov <mikalay.naydzenau@gmail.com>
2022-07-14 17:29:29 +0300

    fix: button functionality fixes

--- main.js
+++ main.js
@@ -3,45 +3,51 @@ let completedList = document.getElementById('completedTasks');
 let form = document.querySelector('form');
 let taskClone = document.querySelector('.list-group-item').cloneNode(true);
 
-let completeButtons = document.getElementsByClassName('btn-success');
-let editButtons = document.getElementsByClassName('btn-info');
-let deleteButtons = document.getElementsByClassName('btn-danger');
+let completeButtons = Array.from(document.getElementsByClassName('btn-success'));
+let editButtons = Array.from(document.getElementsByClassName('btn-info'));
+let deleteButtons = Array.from(document.getElementsByClassName('btn-danger'));
 
 let inputTitle = document.getElementById('inputTitle');
 let inputText = document.getElementById('inputText');
 let priorityInputs = document.querySelectorAll('input[type="radio"]');
 
-for (item of completeButtons) {
-    item.addEventListener('click', completeTask);
-}
 
-for (item of deleteButtons) {
-    item.addEventListener('click', deleteTask);
-}
+todoList.addEventListener('click', completeTask); 
+
+completedList.addEventListener('click', deleteTask); 
+todoList.addEventListener('click', deleteTask);
 
 form.addEventListener('submit', addTask);
 
 function completeTask(event) {
-    let task = event.target.closest('.list-group-item');
-    let dropDown = event.target.closest(".dropdown");
 
-    dropDown.querySelector('.btn-success').style.display = 'none';
-    dropDown.querySelector('.btn-info').style.display = 'none';
+    if (event.target.classList.contains('btn-success')) {
+        let task = event.target.closest('li');
+        let dropDown = event.target.closest(".dropdown");
 
-    completedList.append(task);
+        dropDown.querySelector('.btn-success').style.display = 'none';
+        dropDown.querySelector('.btn-info').style.display = 'none';
+
+        completedList.append(task);
+    }
 }
 
 function deleteTask(event) {
-    let task = event.target.closest('.list-group-item');
 
-    task.remove();
+    if (event.target.classList.contains('btn-danger')) {
+        let task = event.target.closest('li');
+
+        task.remove();
+    }
 }
 
 function addTask(event) {
 
     event.preventDefault();
 
-    let task = taskClone;
+    let task = document.createElement('li');
+    task.innerHTML = taskClone.innerHTML;
+    task.classList = "list-group-item d-flex w-100 mb-2";
 
     let title = task.querySelector('.mb-1');
     title.textContent = inputTitle.value;
@@ -52,7 +58,7 @@ function addTask(event) {
     inputTitle.value = '';
     inputText.value = '';
 
-    todoList.append(task);
+    todoList.insertAdjacentElement("beforeend", task);
 
     $("#exampleModal").modal("hide");
 
