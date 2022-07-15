97ccb56f97122f3f8ba4beb9311413a9464febbe
Nikolay Naidenov <mikalay.naydzenau@gmail.com>
2022-07-15 08:06:06 +0300

    add: implementing date of adding functionality

--- main.js
+++ main.js
@@ -3,16 +3,14 @@ let completedList = document.getElementById('completedTasks');
 let form = document.querySelector('form');
 let taskClone = document.querySelector('.list-group-item').cloneNode(true);
 
-let completeButtons = Array.from(document.getElementsByClassName('btn-success'));
-let editButtons = Array.from(document.getElementsByClassName('btn-info'));
-let deleteButtons = Array.from(document.getElementsByClassName('btn-danger'));
-
 let inputTitle = document.getElementById('inputTitle');
 let inputText = document.getElementById('inputText');
 let priorityInputs = document.querySelectorAll('input[type="radio"]');
 
 
-todoList.addEventListener('click', completeTask); 
+todoList.addEventListener('click', completeTask);
+
+todoList.addEventListener('click', editTask);
 
 completedList.addEventListener('click', deleteTask); 
 todoList.addEventListener('click', deleteTask);
@@ -29,6 +27,12 @@ function completeTask(event) {
         dropDown.querySelector('.btn-info').style.display = 'none';
 
         completedList.append(task);
+    }
+}
+
+function editTask(event) {
+    if (event.target.classList.contains('btn-info')) {
+        $('#myModal').modal('show');
     }
 }
 
@@ -55,8 +59,35 @@ function addTask(event) {
     let text = task.querySelector('p');
     text.textContent = inputText.value;
 
+    let priority = task.querySelector('small');
+
+
+    for (item of priorityInputs) {
+        if (item.checked) {
+            priority.textContent = item.value + " priority";
+        }
+    }
+
+    let date = new Date();
+
+    let day = (date.getDate() < 10) ? ("0" + date.getDate()) : date.getDate();
+    let month = ((date.getMonth() + 1) < 10) ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
+    let minutes = ((date.getMinutes()) < 10) ? ("0" + date.getMinutes()) : date.getMinutes();
+
+    let time = date.getHours() + ":" + minutes + " " + day + "." + month + "." + date.getFullYear();
+
+    let timeOfAdd = date.getTime();
+    task.timeOfAdd = timeOfAdd;
+
+    let dateElement = task.querySelector('.date');
+    dateElement.textContent = time;
+
     inputTitle.value = '';
     inputText.value = '';
+    
+    for (item of priorityInputs) {
+        item.checked = false;
+    }
 
     todoList.insertAdjacentElement("beforeend", task);
 
