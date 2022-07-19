fcf2ce4ebeffd3c3606ed9f816aa1c6dc4b2d9ad
Nikolay Naidenov <mikalay.naydzenau@gmail.com>
2022-07-19 07:58:27 +0300

    code refactoring

--- main.js
+++ main.js
@@ -20,11 +20,6 @@ let sortNewButton = document.getElementById('sortNew');
 let sotrOldButton = document.getElementById('sortOld');
 let nightButton = document.getElementById('night');
 
-/*$('#exampleModal').on('hide.bs.modal', function (e) {
-    alert(1);
-});*/
-
-
 todoList.addEventListener('click', completeTask);
 todoList.addEventListener('click', editTask);
 todoList.addEventListener('click', deleteTask);
@@ -42,6 +37,7 @@ nightButton.addEventListener('click', activateNightMode);
 function completeTask(event) {
 
     if (event.target.classList.contains('btn-success')) {
+
         let task = event.target.closest('li');
         let dropDown = event.target.closest(".dropdown");
 
@@ -57,19 +53,19 @@ function completeTask(event) {
 
 function editTask(event) {
     if (event.target.classList.contains('btn-info')) {
+
         $("#exampleModal").modal("show");
 
         taskToEdit = event.target.closest('li');
 
         button.textContent = "Edit";
-
     }
-
 }
 
 function deleteTask(event) {
 
     if (event.target.classList.contains('btn-danger')) {
+
         let task = event.target.closest('li');
 
         if (event.target.closest('#currentTasks')) {
@@ -93,30 +89,19 @@ function submitFormEdit(event) {
 
         let title = task.querySelector('.mb-1');
         title.textContent = inputTitle.value;
-        
+    
         let text = task.querySelector('p');
         text.textContent = inputText.value;
 
         let priority = task.querySelector('small');
-
-
         for (item of priorityInputs) {
             if (item.checked) {
                 priority.textContent = item.value + " priority";
             }
         }
 
-        let date = new Date();
-
-        //Components of date calculation
-
-        let day = (date.getDate() < 10) ? ("0" + date.getDate()) : date.getDate();
-        let month = ((date.getMonth() + 1) < 10) ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
-        let minutes = ((date.getMinutes()) < 10) ? ("0" + date.getMinutes()) : date.getMinutes();
-
-        let time = date.getHours() + ":" + minutes + " " + day + "." + month + "." + date.getFullYear();
-
-        let timeOfAdd = date.getTime();
+        let time = getDateOfAdd();
+        let timeOfAdd = (new Date()).getTime();
         task.timeOfAdd = timeOfAdd;
 
         let dateElement = task.querySelector('.date');
@@ -124,22 +109,12 @@ function submitFormEdit(event) {
 
         task.style.backgroundColor = inputColor.value;
 
-        
         //"Darkness" of the color calculation
         if (parseInt(inputColor.value.slice(1), 16) < 8e6) {
             task.style.color = "#ffffff";
         }
 
-        //Set inputs to default values
-
-        inputTitle.value = '';
-        inputText.value = '';
-        inputColor.value = "#ffffff";
-        
-        for (item of priorityInputs) {
-            item.checked = false;
-        }
-
+        setInputsToEmpty();
 
         $("#exampleModal").modal("hide");
 
@@ -155,9 +130,9 @@ function submitFormAdd(event) {
     if (button.textContent == "Add task") {
 
         todoList.previousElementSibling.innerHTML = "ToDo (" + ++taskCounter + ")";
+
         let title = inputTitle.value;
         let text = inputText.value;
-
         let priority;
 
         for (item of priorityInputs) {
@@ -165,67 +140,48 @@ function submitFormAdd(event) {
                 priority = item.value;
             }
         }
-
-        let date = new Date();
-
-        //Components of date calculation
-
-        let day = (date.getDate() < 10) ? ("0" + date.getDate()) : date.getDate();
-        let month = ((date.getMonth() + 1) < 10) ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
-        let minutes = ((date.getMinutes()) < 10) ? ("0" + date.getMinutes()) : date.getMinutes();
-
-        let time = date.getHours() + ":" + minutes + " " + day + "." + month + "." + date.getFullYear();
+        
+        let time = getDateOfAdd();
 
         let taskHTML = `<div class="w-100 mr-2">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">${title}</h5>
-                            <div>
-                                <small class="mr-2">${priority} priority</small>
-                                <small class="date">${time}</small>
-                            </div>
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">${title}</h5>
+                                <div>
+                                    <small class="mr-2">${priority} priority</small>
+                                    <small class="date">${time}</small>
+                                </div>
 
+                            </div>
+                            <p class="mb-1 w-100">${text}</p>
                         </div>
-                        <p class="mb-1 w-100">${text}</p>
-                    </div>
-                    <div class="dropdown m-2 dropleft">
-                        <button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v"></i>
-                        </button>
-                        <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
-                            <button type="button" class="btn btn-success w-100">Complete</button>
-                            <button type="button" class="btn btn-info w-100 my-2">Edit</button>
-                            <button type="button" class="btn btn-danger w-100">Delete</button>
-                        </div>
-                    </div>`
+                        <div class="dropdown m-2 dropleft">
+                            <button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </button>
+                            <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
+                                <button type="button" class="btn btn-success w-100">Complete</button>
+                                <button type="button" class="btn btn-info w-100 my-2">Edit</button>
+                                <button type="button" class="btn btn-danger w-100">Delete</button>
+                            </div>
+                        </div>`
 
         task = document.createElement('li');
         task.className = "list-group-item d-flex w-100 mb-2";
         task.innerHTML = taskHTML;
 
-        let timeOfAdd = date.getTime();
-        task.timeOfAdd = timeOfAdd;
-
-
         task.style.backgroundColor = inputColor.value;
 
-        
         //"Darkness" of the color calculation
         if (parseInt(inputColor.value.slice(1), 16) < 8e6) {
             task.style.color = "#ffffff";
         }
 
-        //Set inputs to default values
+        setInputsToEmpty();
 
-        inputTitle.value = '';
-        inputText.value = '';
-        inputColor.value = "#ffffff";
-        
-        for (item of priorityInputs) {
-            item.checked = false;
-        }
+        let timeOfAdd = (new Date()).getTime();
+        task.timeOfAdd = timeOfAdd;
 
         todoList.append(task);
-        
 
         $("#exampleModal").modal("hide");
     }
@@ -272,5 +228,29 @@ function activateNightMode(event) {
     document.querySelector('.navbar').classList.toggle('night-nav');
 }
 
+
+function getDateOfAdd() {
+
+    let date = new Date();
+
+    let day = (date.getDate() < 10) ? ("0" + date.getDate()) : date.getDate();
+    let month = ((date.getMonth() + 1) < 10) ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
+    let minutes = ((date.getMinutes()) < 10) ? ("0" + date.getMinutes()) : date.getMinutes();
+
+    let time = date.getHours() + ":" + minutes + " " + day + "." + month + "." + date.getFullYear();
+
+    return time;
+}
+
+function setInputsToEmpty() {
+
+    inputTitle.value = '';
+    inputText.value = '';
+    inputColor.value = "#ffffff";
+    
+    for (item of priorityInputs) {
+        item.checked = false;
+    }
+}
 
 
